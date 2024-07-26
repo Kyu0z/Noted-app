f500bffcaa65819d9bdc4e168eaa8b889897a495
Duy Khai <duykhaidevfe@gmail.com>
2024-07-26 16:42:46 +0700

    feat: CRUD with group-by-category

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,18 +6,23 @@ import {
   Text,
   TouchableOpacity,
   Keyboard,
+  ScrollView,
 } from 'react-native';
-import DraggableFlatList from 'react-native-draggable-flatlist';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import DraggableFlatList, {
+  RenderItemParams,
+} from 'react-native-draggable-flatlist';
 
 interface Note {
   id: string;
   content: string;
+  category: string;
 }
 
 const HomeScreen: React.FC = () => {
   const [notes, setNotes] = useState<Note[]>([]);
   const [currentNote, setCurrentNote] = useState<string>('');
+  const [currentCategory, setCurrentCategory] = useState<string>('');
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [editingNoteId, setEditingNoteId] = useState<string | null>(null);
 
@@ -25,9 +30,14 @@ const HomeScreen: React.FC = () => {
     if (currentNote.trim() !== '') {
       setNotes([
         ...notes,
-        { id: Math.random().toString(), content: currentNote },
+        {
+          id: Math.random().toString(),
+          content: currentNote,
+          category: currentCategory,
+        },
       ]);
       setCurrentNote('');
+      setCurrentCategory('');
       Keyboard.dismiss(); // Đóng bàn phím
     }
   };
@@ -36,6 +46,7 @@ const HomeScreen: React.FC = () => {
     const noteToEdit = notes.find((note) => note.id === noteId);
     if (noteToEdit) {
       setCurrentNote(noteToEdit.content);
+      setCurrentCategory(noteToEdit.category);
       setIsEditing(true);
       setEditingNoteId(noteId);
     }
@@ -44,10 +55,13 @@ const HomeScreen: React.FC = () => {
   const updateNote = () => {
     setNotes(
       notes.map((note) =>
-        note.id === editingNoteId ? { ...note, content: currentNote } : note
+        note.id === editingNoteId
+          ? { ...note, content: currentNote, category: currentCategory }
+          : note
       )
     );
     setCurrentNote('');
+    setCurrentCategory('');
     setIsEditing(false);
     setEditingNoteId(null);
     Keyboard.dismiss(); // Đóng bàn phím
@@ -59,35 +73,60 @@ const HomeScreen: React.FC = () => {
 
   const clearInput = () => {
     setCurrentNote('');
+    setCurrentCategory('');
   };
 
-  const renderItem = ({ item, drag }: { item: Note; drag: () => void }) => (
-    <TouchableOpacity
-      style={[
-        styles.noteRow,
-        notes.indexOf(item) === notes.length - 1 && styles.lastNoteRow,
-      ]}
-      onLongPress={drag} // Kéo và thả
-    >
-      <Text style={styles.noteIndex}>{notes.indexOf(item) + 1}. </Text>
-      <View style={styles.noteContainer}>
-        <Text style={styles.noteContent}>{item.content}</Text>
-        <View style={styles.actions}>
-          <TouchableOpacity onPress={() => editNote(item.id)}>
-            <Icon name='edit' size={20} style={styles.editButton} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => deleteNote(item.id)}>
-            <Icon name='trash' size={20} style={styles.deleteButton} />
-          </TouchableOpacity>
+  const renderItem = ({ item, drag, isActive }: RenderItemParams<Note>) => {
+    const index = notes.findIndex((note) => note.id === item.id);
+    return (
+      <TouchableOpacity
+        style={[
+          styles.noteRow,
+          index === notes.length - 1 && styles.lastNoteRow,
+          { backgroundColor: isActive ? '#eaeaeaS' : '#000' },
+        ]}
+        onLongPress={drag}
+      >
+        <Text style={styles.noteIndex}>{index + 1}. </Text>
+        <View style={styles.noteContainer}>
+          <Text style={styles.noteContent}>{item.content}</Text>
+          <View style={styles.actions}>
+            <TouchableOpacity onPress={() => editNote(item.id)}>
+              <Icon name='edit' size={20} style={styles.editButton} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => deleteNote(item.id)}>
+              <Icon name='trash' size={20} style={styles.deleteButton} />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    </TouchableOpacity>
-  );
+      </TouchableOpacity>
+    );
+  };
+
+  const groupedNotes = notes.reduce((groups: any, note: any) => {
+    const category = note.category || 'Uncategorized';
+    if (!groups[category]) {
+      groups[category] = [];
+    }
+    groups[category].push(note);
+    return groups;
+  }, {});
+
+  const sections = Object.keys(groupedNotes).map((category) => ({
+    title: category,
+    data: groupedNotes[category],
+  }));
 
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Create a new todo...</Text>
       <View style={styles.inputContainer}>
+        <TextInput
+          style={[styles.input, styles.categoryInput]}
+          placeholder='Enter type'
+          value={currentCategory}
+          onChangeText={setCurrentCategory}
+        />
         <TextInput
           style={styles.input}
           placeholder='Enter note'
@@ -111,20 +150,20 @@ const HomeScreen: React.FC = () => {
           </TouchableOpacity>
         </View>
       </View>
-      {notes.length > 0 && (
-        <View style={styles.listContainer}>
-          <DraggableFlatList
-            data={notes}
-            keyExtractor={(item) => item.id}
-            renderItem={renderItem}
-            onDragEnd={({ data }) => setNotes(data)}
-            contentContainerStyle={styles.flatListContent}
-            ListHeaderComponent={
-              <Text style={styles.subHeading}>List Note</Text>
-            }
-          />
-        </View>
-      )}
+      <ScrollView style={{ flex: 1 }}>
+        {sections.map((section) => (
+          <View style={styles.listContainer} key={section.title}>
+            <Text style={styles.subHeading}>{section.title}</Text>
+            <DraggableFlatList
+              data={section.data}
+              keyExtractor={(item) => item.id}
+              renderItem={renderItem}
+              onDragEnd={({ data }) => setNotes(data)}
+              contentContainerStyle={styles.flatListContent}
+            />
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 };
@@ -150,7 +189,7 @@ const styles = StyleSheet.create({
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 18,
+    marginBottom: 38,
   },
   input: {
     flex: 1,
@@ -159,6 +198,9 @@ const styles = StyleSheet.create({
     padding: 10,
     color: '#fff',
     borderRadius: 8,
+  },
+  categoryInput: {
+    marginRight: 10,
   },
   buttonContainer: {
     flexDirection: 'row',
@@ -183,15 +225,21 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   listContainer: {
-    flex: 1,
-  },
-  flatListContent: {
+    // flex: 1,
     borderWidth: 1,
     borderColor: 'rgba(255, 255, 255, 0.2)',
     borderRadius: 8,
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     padding: 20,
     paddingTop: 4,
+  },
+  flatListContent: {
+    // borderWidth: 1,
+    // borderColor: 'rgba(255, 255, 255, 0.2)',
+    // borderRadius: 8,
+    // backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    // padding: 20,
+    // paddingTop: 4,
   },
   noteRow: {
     flexDirection: 'row',
